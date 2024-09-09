6df20af0641c9ba47f73fac5824e75d7fefde9f6
tokosaniya09 <tokosaniya09@gmail.com>
2024-09-09 10:48:11 +0530

    Basic functionalties of notes is added

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const { log } = require('console');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    fs.readdir(`./files`, (err, files) => {
+        if (err) {
+            return res.status(500).send("Unable to scan directory");
+        }
+        res.render("index", { files });  
+    });
+});
+
+app.get("/files/:filename", (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, fileData) => {
+        if (err) {
+            return res.status(500).send("Unable to read file");
+        }
+        res.render("show", {filename : req.params.filename, fileData});
+    });
+});
+
+app.get("/edit/:filename", (req, res) => {
+    res.render("edit", {filename : req.params.filename});
+});
+
+app.post("/edit", (req, res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, err => {
+        res.redirect("/");
+    });
+});
+
+app.post("/create", (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
+        res.redirect("/")
+    })
+}) 
+
+app.get("/editContent/:filename", (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, fileData) => {
+        if (err) {
+            return res.status(500).send("Unable to read file");
+        }
+        res.render("editContent", {filename : req.params.filename, fileData});
+    });
+});
+
+app.post("/editContent", (req, res) => {
+    fs.writeFile(`./files/${req.body.filename}`, req.body.previousContent, (err) => {
+        if (err) {
+            console.error("Error writing file:", err);
+        } else {
+            res.redirect(`./files/${req.body.filename}`);
+        }
+    });
+
+    console.log(req.body);
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
